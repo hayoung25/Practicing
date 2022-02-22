3b284e8bca3f6fe326b1559d88aeecf43425fafd
Hyeo8 <hyeo8@wisc.edu>
2022-02-21 21:15:46 -0600

    fix bug

--- Mandala_art_planner/app.js
+++ Mandala_art_planner/app.js
@@ -3,30 +3,38 @@ const container = document.querySelector(".container");
 const containerRow = document.querySelector(".container-row");
 const parentBlock = document.querySelector(".parentBlock");
 const row = document.querySelector(".row");
+const block = document.querySelector(".block");
+
 
 // Event Listeners
-const clickEvent = container.addEventListener('click', createInput);
-// containerRow.addEventListener('click', (e) => stopPropagation);
+container.addEventListener('click', createInput);
 
 
-//functions
+// Functions
 function createInput(e) {
-    const newInput = document.createElement('input');
+    const input = document.createElement('input');
     const block = e.target;
-
+    const paragraph= document.createElement('p')
+    
+    console.log(block.classList[0])
+    // Create input
     if (block.classList[0] === "block") {
-        block.innerHTML = "";
-        block.appendChild(newInput);
-        newInput.classList.add("new-input");
-        console.log(clickEvent)
-    }  
+        block.innerHTML =""
+        block.appendChild(input);
+        block.appendChild(paragraph);
+        input.classList.add("new-input");
+        paragraph.classList.add("new-paragraph");
+    } 
 
-    newInput.addEventListener('keypress', (e) => {
+    // Add value in a box
+    input.addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {
-            const inputValue = document.querySelector(".new-input").value;
-            console.log(inputValue);
-            newInput.remove;
-            block.innerHTML = inputValue;
+            const newInput = block.querySelector('.new-input');
+            const newParagraph = block.querySelector('.new-paragraph');
+            const inputValue = newInput.value;
+            block.removeChild(newInput)
+            newParagraph.innerHTML = inputValue;
         }
     })    
 }
+
